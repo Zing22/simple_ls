7376c7636c03ee24f87fb01eb2b3c197538d9c23
Zing <love.super.man@qq.com>
2015-02-03 11:19:37 +0800

    did some modifications

--- ls.c
+++ ls.c
@@ -3,7 +3,6 @@
 #include    <unistd.h>
 #include    <dirent.h>
 #include    <string.h>
-#include    <malloc.h>
 #include    <sys/param.h>
 #include    <sys/stat.h>
 #include    <grp.h>
@@ -12,10 +11,7 @@
 #include    <stddef.h>
 #include    <time.h>
 
-#define     MAXBUFSIZE PATH_MAX
-#define     INITSIZE 2
-#define     MAXFILENAME PATH_MAX 
-#define		INITNUMBER 4
+#define     INITNUMBER 4
 
 //	各个数据的宽度
 unsigned int w_nlink,w_uid,w_gid,w_size,w_time,w_name;
@@ -25,7 +21,7 @@ unsigned int isLong = 1;
 struct Filelist
 {
 	char *name;
-	char *permission;
+	char permission[11];
 	int filelength;
 	int nlink;
 	char *uid,*gid;
@@ -44,30 +40,34 @@ struct stat buf;
 struct passwd *data1 = NULL;
 struct group *data2 = NULL;
 struct tm *mtm = NULL;
-char nameLen[50];
 unsigned int tempLen=0;
 //读取文件属性数据
-int readAll(const char *path,char *filename,int filenum)
+int readAll(char *filename,int filenum)
 {
-	//获取文件路径，存入str中
-	char *str = (char*)malloc((strlen(path)+strlen(filename)+2)*sizeof(char));
-	memset(str,'\0',(strlen(path)+strlen(filename)+2)*sizeof(char));
-	strcpy(str,path);
-	strcat(str,"/");
-	strcat(str,filename);
-	strcat(str,"\0");
 	//stat函数获取文件属性
-	stat(str,&buf);
+	stat(filename,&buf);
 
 	//size of file
 	files[filenum]->filelength = buf.st_size;
-	tempLen = sprintf(nameLen,"%i",files[filenum]->filelength);
+	tempLen = snprintf(NULL,0,"%i",files[filenum]->filelength);
 	if(tempLen > w_size) w_size = tempLen;
 	//permission of file
-	files[filenum]->permission = (char*)malloc(11*sizeof(char));
 	memset(files[filenum]->permission,'-',11*sizeof(char));
-	if(S_ISDIR(buf.st_mode))
-		files[filenum]->permission[0] = 'd';
+	if(S_ISCHR(buf.st_mode))
+		files[filenum]->permission[0]='c';
+	else if(S_ISBLK(buf.st_mode))
+		files[filenum]->permission[0]='b';
+	else if(S_ISLNK(buf.st_mode))
+		files[filenum]->permission[0]='l';
+	else if(S_ISSOCK(buf.st_mode))
+		files[filenum]->permission[0]='s';
+	else if(S_ISDIR(buf.st_mode))
+		files[filenum]->permission[0]='d';
+	else if(S_ISREG(buf.st_mode))
+		files[filenum]->permission[0]='-';
+	else
+		files[filenum]->permission[0]='p';
+		
 	if(buf.st_mode & S_IRUSR) files[filenum]->permission[1] = 'r';
 	if(buf.st_mode & S_IWUSR) files[filenum]->permission[2] = 'w';
 	if(buf.st_mode & S_IXUSR) files[filenum]->permission[3] = 'x';
@@ -77,10 +77,14 @@ int readAll(const char *path,char *filename,int filenum)
 	if(buf.st_mode & S_IROTH) files[filenum]->permission[7] = 'r';
 	if(buf.st_mode & S_IWOTH) files[filenum]->permission[8] = 'w';
 	if(buf.st_mode & S_IXOTH) files[filenum]->permission[9] = 'x';
+
+	if(buf.st_mode & S_ISUID) files[filenum]->permission[3] = 's';
+	if(buf.st_mode & S_ISGID) files[filenum]->permission[6] = 's';
+	if(buf.st_mode & S_ISVTX) files[filenum]->permission[9] = 't';
 	files[filenum]->permission[10] = '\0';
 	//nlink of file
 	files[filenum]->nlink = buf.st_nlink;
-	tempLen = sprintf(nameLen,"%i",files[filenum]->nlink);
+	tempLen = snprintf(NULL,0,"%i",files[filenum]->nlink);
 	if(tempLen > w_nlink) w_nlink = tempLen;
 	//UID & GID of file
 	data1 = getpwuid(buf.st_uid);
@@ -91,10 +95,12 @@ int readAll(const char *path,char *filename,int filenum)
 	if(strlen(files[filenum]->gid) > w_gid) w_gid = strlen(files[filenum]->gid);
 	//mtime of file
 	mtm = localtime(&buf.st_mtim.tv_sec);
-	strftime(files[filenum]->mtime,128,"%m月 %e %R",mtm);
+	if(strcmp(getenv("LANG"),"zh_CN.UTF-8")==0)
+		strftime(files[filenum]->mtime,128,"%m %e %R",mtm);
+	else
+		strftime(files[filenum]->mtime,128,"%b %e %R",mtm);
 	if(strlen(files[filenum]->mtime) > w_time) w_time = strlen(files[filenum]->mtime);
 
-	free(str);
 	return 0;
 }
 
@@ -104,7 +110,6 @@ int readFile(const char *path)
 	DIR	*dir = NULL;
 	struct dirent *ptr = NULL;
 	dir = opendir(path);
-	size_t sum = 0;				//已经存储的文件数量
 
 	while((ptr=readdir(dir)) != NULL)
 	{
@@ -120,10 +125,9 @@ int readFile(const char *path)
 		memset(files[fileNum]->name,'\0',(length+1)*sizeof(char));
 		strncpy(files[fileNum]->name,ptr->d_name,length);
 		if(isLong)
-			readAll(path,files[fileNum]->name,fileNum);			//读写其他文件属性数据
+			readAll(files[fileNum]->name,fileNum);			//读写其他文件属性数据
 		++fileNum;
-		sum = fileNum * sizeof(char*);
-		if( fileNum >= maxNum/2 )				//使结构体组指针内存空间翻倍
+		if( fileNum == maxNum )				//使结构体组指针内存空间翻倍
 		{
 			maxNum *= 2;
 			files = (struct Filelist**)realloc(files,sizeof(struct Filelist*)*maxNum);
@@ -142,8 +146,8 @@ int long_printf()
 	int i;
 	for( i = 0 ; i < fileNum ; ++i )				//输出并且free内存空间
 	{
-		printf("%s %*i %*s %*s %*i %*s %-*s\n",files[i]->permission, w_nlink, files[i]->nlink, w_uid, files[i]->uid, w_gid, files[i]->gid, w_size, files[i]->filelength, w_time, files[i]->mtime, w_name, files[i]->name);
-		free(files[i]->permission);
+		printf("%s %*i %*s %*s %*i %*s %-*s\n",files[i]->permission, w_nlink, files[i]->nlink, w_uid, files[i]->uid,
+					w_gid, files[i]->gid, w_size, files[i]->filelength, w_time, files[i]->mtime, w_name, files[i]->name);
 		free(files[i]->name);
 		free(files[i]);
 	}
@@ -158,7 +162,6 @@ int simple_print()
 	{
 		printf("%-*s",w_name+2,files[i]->name);
 		free(files[i]->name);
-//		free(files[i]->permission);
 		free(files[i]);
 		if( (i+1)%10==0 )
 			printf("\n");
@@ -171,12 +174,11 @@ int simple_print()
 int main(int argc, char *argv[])
 {
 	files = (struct Filelist **)malloc(sizeof(struct Filelist*)*INITNUMBER);//初始化结构体组指针的内存空间
-	char currentPath[MAXBUFSIZE];
-	getcwd(currentPath,MAXBUFSIZE);			//读取当前路径
-	const char para[] = "-l";
+	char currentPath[PATH_MAX];
+	getcwd(currentPath,PATH_MAX);			//读取当前路径
 	if( argc > 1 )
 	{
-		if(strcmp(argv[1],para)==0)
+		if(strcmp(argv[1],"-l")==0)
 		{
 			readFile(currentPath);					//读取数据
 			printf("current path : %s\n",currentPath);
@@ -193,5 +195,5 @@ int main(int argc, char *argv[])
 		simple_print();
 	}
 	free(files);							//释放结构体组指针所占的内存空间
-	exit(0);
+	return 0;
 }
